482b15117227860a70237f3ab478f4ccd44d52ac
Pilllico <douglas1995@hotmail.fr>
2020-11-02 15:14:44 +0100

    Tried to generate terrain and smooth it

---
+++ Assets/Scripts/GenerateEntities.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEntities : MonoBehaviour
+{
+    public Terrain terrain;
+    public GameObject player;
+    public GameObject foe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.transform.position = new Vector3(-500, -500, 10);
+        Instantiate(player);
+        Debug.Log("Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
